fa3eb0e105a0768017ccf5de0f18fe7339ea8a2f
Eric Davis <edavis@eresources.com>
2020-05-02 15:19:00 -0700

    Simplify and separate Site and MWA classes

--- pullman.py
+++ pullman.py
@@ -1,57 +1,52 @@
 #!/usr/bin/env python
 
-import frontmatter
-from itertools import count
+import json
+import uuid
+import pprint
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+import frontmatter
 
 # h/t <https://gist.github.com/brentsimmons/9398899>
 # h/t <http://1998.xmlrpc.com/metaWeblogApi.html>
+
+class Site(object):
+    def __init__(self, site_id):
+        self.site_id = site_id
+        self.site_root = "/Users/eric/Dropbox (Personal)/Websites/%s/" % (site_id,)
+
+    def NewPost(self, struct, publish):
+        guid = self.GeneratePostID()
+        struct['postid'] = guid
+        (blog_id, post_id) = guid.split(':')
+        with open(post_id, 'w') as fp:
+            json.dump(struct, fp)
+        return guid
+
+    def GetPost(self, post_id):
+        with open(post_id) as fp:
+            return json.load(fp)
+
+    def GeneratePostID(self):
+        return "%s:%s.md" % (self.site_id, uuid.uuid4())
+
 class MetaWeblogAPI(object):
-    def __init__(self):
-        self.post_id_counter = count(1)
-        self.posts = {}
+    def NewPost(self, site_id, username, password, struct, publish):
+        site = Site(site_id)
+        return site.NewPost(struct, publish)
 
-    def getRecentPosts(self, blog_id, username, password, post_count):
-        recent_posts = []
-        for post_id in self.posts.keys():
-            recent_posts.append(self.posts[post_id])
-        return recent_posts
-
-    def getCategories(self, blog_id, username, password):
-        return []
-
-    def getPost(self, post_id, username, password):
-        return self.posts[post_id]
-
-    def newPost(self, blog_id, username, password, struct, publish):
-        post_id = next(self.post_id_counter)
-        struct.update({'postid': post_id})
-        self.posts[post_id] = struct
-        return post_id
-
-    def editPost(self, post_id, username, password, struct, publish):
-        self.posts[post_id] = struct
-        return True
-
-    def deletePost(self, appkey, post_id, username, password, publish):
-        del self.posts[int(post_id)]
-        return True
-
-    # h/t <https://micro.blog/elliot/217975>
-    # getUserInfo
+    def GetPost(self, guid, username, password):
+        (site_id, post_id) = guid.split(':')
+        site = Site(site_id)
+        return site.GetPost(post_id)
 
 def main(args):
     server = SimpleXMLRPCServer(('localhost', 1894))
     server.register_introspection_functions()
 
-    mwa = MetaWeblogAPI()
-    server.register_function(mwa.newPost, 'metaWeblog.newPost')
-    server.register_function(mwa.getPost, 'metaWeblog.getPost')
-    server.register_function(mwa.editPost, 'metaWeblog.editPost')
-    server.register_function(mwa.deletePost, 'blogger.deletePost')
-    server.register_function(mwa.getRecentPosts, 'metaWeblog.getRecentPosts')
-    server.register_function(mwa.getCategories, 'metaWeblog.getCategories')
-
+    metaweblog = MetaWeblogAPI()
+    server.register_function(metaweblog.NewPost, 'metaWeblog.newPost')
+    server.register_function(metaweblog.GetPost, 'metaWeblog.getPost')
     server.serve_forever()
 
 if __name__ == '__main__':
